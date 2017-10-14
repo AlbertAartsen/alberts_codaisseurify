07078603b12d3c199916040fbaf246d7cd451ea1
AlbertAartsen <albertusaartsen@gmail.com>
2017-10-14 14:04:33 +0200

    Artist & Song model create

---
+++ spec/factories/artists.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :artist do
+    first_name "MyString"
+    last_name "MyString"
+    genre "MyString"
+    nationality "MyString"
+  end
+end
---
+++ spec/factories/songs.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :song do
+    name "MyString"
+    length "9.99"
+    year 1
+  end
+end
