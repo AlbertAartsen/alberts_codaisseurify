59de1529b9ab8d9651f13d77c8aa23d6ae174cb9
AlbertAartsen <albertusaartsen@gmail.com>
2017-10-14 20:42:13 +0200

    songs added to artist showpage

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,6 +1,45 @@
 class SongsController < ApplicationController
 
+def index
+  @songs = current_artist.songs
+end
 
+def show
+  @song = Song.find(params[:id])
+end
+
+def new
+  @song = current_artist.songs.build
+end
+
+def create
+  @songs = current_artist.songs.build(song_params)
+
+    @song = Song.new(song_params)
+
+    if @song.save
+      redirect_to @song
+    else
+      render 'new'
+    end
+end
+
+def edit
+  @song = Song.find(params[:id])
+end
+
+def destroy
+    @Song = Song.find(params[:id])
+
+    @Song.destroy
+
+    redirect_to song_path
+  end
+
+
+  private
+
+  song_params = params.require(:song).permit(:name, :length, :year, :nationality)
 
 
 end
