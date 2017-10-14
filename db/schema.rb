9d10da6f28d66da0bf0c70a1d51395f6cf04f87d
AlbertAartsen <albertusaartsen@gmail.com>
2017-10-14 14:12:29 +0200

    one-to-many Artist to Songs relationship create

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171014120337) do
+ActiveRecord::Schema.define(version: 20171014121113) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,9 @@ ActiveRecord::Schema.define(version: 20171014120337) do
     t.integer "year"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "artist_id"
+    t.index ["artist_id"], name: "index_songs_on_artist_id"
   end
 
+  add_foreign_key "songs", "artists"
 end
