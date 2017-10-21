c05d34109c7f35ac7280663048e585f0e56c8737
AlbertAartsen <albertusaartsen@gmail.com>
2017-10-21 14:35:54 +0200

    add song with ajax feature test

---
+++ spec/features/add_songs_spec.rb
@@ -0,0 +1,10 @@
+require "rails_helper"
+
+feature "Manage tasks", js: true do
+  scenario "add composition" do
+    visit artists_path(@artist)
+      fill_in "artist_song_name", with: "Fur Elise"
+        page.execute_script("$('form').submit()")
+      expect(page).to have_content("Fur Elise")
+  end
+end
